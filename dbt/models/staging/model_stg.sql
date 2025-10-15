0894dbd4637c2635e848998d31bad638150538a9
carolnavya <carolnavya1999@gmail.com>
2025-10-14 23:58:25 -0700

    update dbt

--- dbt/models/staging/model_stg.sql
+++ dbt/models/staging/model_stg.sql
@@ -1,27 +1,35 @@
+{{ config(
+    alias='dams_base',
+    materialized='incremental'
+) }}
 
-/*
-    Welcome to your first dbt model!
-    Did you know that you can also configure models directly within SQL files?
-    This will override configurations stated in dbt_project.yml
-
-    Try changing "table" to "view" below
-*/
-
-{{ config(materialized='table') }}
-
-with source_data as (
-
-    select 1 as id
-    union all
-    select null as id
-
+WITH parsed AS (
+    SELECT
+        _AIRBYTE_RAW_ID,
+        ID,
+        TYPE,
+        GEOMETRY   AS geometry_json,
+        PROPERTIES AS properties_json
+    FROM {{ source('dams_esri', 'DAMS_RAW') }}
+),
+extracted AS (
+    SELECT
+        _AIRBYTE_RAW_ID,
+        ID,
+        TYPE,
+        geometry_json:type::string AS geometry_type,
+        geometry_json:coordinates[0]::float AS longitude,
+        geometry_json:coordinates[1]::float AS latitude,
+        properties_json:OBJECTID::number AS object_id,
+        properties_json:IncidentName::string AS incident_name,
+        properties_json:FireCause::string AS fire_cause,
+        properties_json:POOCounty::string AS county,
+        properties_json:POOState::string AS state,
+        properties_json:UniqueFireIdentifier::string AS unique_fire_id,
+        properties_json:GlobalID::string AS global_id,
+        properties_json:FireDiscoveryDateTime::number AS fire_discovery_ts,
+        TO_TIMESTAMP_NTZ(properties_json:FireDiscoveryDateTime::number / 1000) AS fire_discovery_time
+    FROM parsed
 )
+SELECT * FROM extracted
 
-select *
-from source_data
-
-/*
-    Uncomment the line below to remove records with null `id` values
-*/
-
--- where id is not null
