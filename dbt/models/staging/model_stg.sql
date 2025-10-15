8c880c4773ef5b7470b29af82a3f4f74f01944a2
carolnavya <carolnavya1999@gmail.com>
2025-10-15 00:02:47 -0700

    update dbt

--- dbt/models/staging/model_stg.sql
+++ dbt/models/staging/model_stg.sql
@@ -20,15 +20,8 @@ extracted AS (
         geometry_json:type::string AS geometry_type,
         geometry_json:coordinates[0]::float AS longitude,
         geometry_json:coordinates[1]::float AS latitude,
-        properties_json:OBJECTID::number AS object_id,
-        properties_json:IncidentName::string AS incident_name,
-        properties_json:FireCause::string AS fire_cause,
-        properties_json:POOCounty::string AS county,
-        properties_json:POOState::string AS state,
-        properties_json:UniqueFireIdentifier::string AS unique_fire_id,
-        properties_json:GlobalID::string AS global_id,
-        properties_json:FireDiscoveryDateTime::number AS fire_discovery_ts,
-        TO_TIMESTAMP_NTZ(properties_json:FireDiscoveryDateTime::number / 1000) AS fire_discovery_time
+        properties_json:AIANNH::string AS aiannh,
+        properties_json:ASSOCIATED_STRUCTURES::string AS associated_structures
     FROM parsed
 )
 SELECT * FROM extracted
